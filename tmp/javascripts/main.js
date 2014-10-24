385fa94510610e4213c91893eb1ece81ef4589ef
Andre Ortiz <andreortiz82@gmail.com>
2014-10-24 14:16:06 -0500

    adding build files

---
+++ tmp/javascripts/main.js
@@ -0,0 +1,9 @@
+(function() {
+  this.main = {
+    init: function() {
+      $(document).foundation();
+      return console.log('Hi, Welcome to my bootstrap project');
+    }
+  };
+
+}).call(this);
